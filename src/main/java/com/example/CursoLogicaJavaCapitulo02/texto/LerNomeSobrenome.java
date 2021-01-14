4f24d2cbc68ad0cce6dbd4053ea0bb6b0f2f7144
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-01-14 13:46:51 -0300

    Criado programa para printar nome e sobrenome digitado pelo usuário no console. Exercicio String

---
+++ src/main/java/com/example/CursoLogicaJavaCapitulo02/texto/LerNomeSobrenome.java
@@ -0,0 +1,23 @@
+package com.example.CursoLogicaJavaCapitulo02.texto;
+
+import java.util.Scanner;
+
+//Crie um programa que lê, do console, o nome e o sobrenome de uma pessoa e depois imprime o nome completo.
+// Para isso, você vai precisar de duas variáveis: a primeira você pode chamar de "nome" e a segunda de "sobrenome".
+// Por último, você vai juntar o nome e o sobrenome em uma única impressão.
+
+public class LerNomeSobrenome {
+
+    public static void main (String [] args){
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Digite o seu nome: ");
+        String nome = scanner.nextLine();
+        System.out.print("Digite o seu sobrenome: ");
+        String sobrenome = scanner.nextLine();
+
+        System.out.print( nome + sobrenome );
+        scanner.close();
+
+    }
+}
