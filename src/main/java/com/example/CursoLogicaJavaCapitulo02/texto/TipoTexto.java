15af6aae1bbbcfe71e307f4efab9cf5d099515f5
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-01-14 13:09:44 -0300

    Criado programa básico para enviar olá no console.Exercicio String

---
+++ src/main/java/com/example/CursoLogicaJavaCapitulo02/texto/TipoTexto.java
@@ -0,0 +1,9 @@
+package com.example.CursoLogicaJavaCapitulo02.texto;
+
+public class TipoTexto {
+
+    public static void main (String [] args){
+        String mensagemSaudacao = "Olá!";
+        System.out.print(mensagemSaudacao);
+    }
+}
