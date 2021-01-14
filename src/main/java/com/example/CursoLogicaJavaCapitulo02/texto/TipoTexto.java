e9ec2fc398d2547a6acc87604a1a6efccf5140e8
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-01-14 13:26:46 -0300

    Criado programa para solicitar nome no console e printar saudacão em java. Exercicio String

--- src/main/java/com/example/CursoLogicaJavaCapitulo02/texto/TipoTexto.java
+++ src/main/java/com/example/CursoLogicaJavaCapitulo02/texto/TipoTexto.java
@@ -1,11 +1,20 @@
 package com.example.CursoLogicaJavaCapitulo02.texto;
 
+import java.util.Scanner;
+
 public class TipoTexto {
 
     //Código pra concatenar variáveis// Isso serve no return
     public static void main (String [] args){
-        String nome = "Simone Santos";
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Digite o seu nome: ");
+
+        //nextLine é pra ler texto
+        String nome = scanner.nextLine();
         String mensagemSaudacao = "Olá, ";
         System.out.print(mensagemSaudacao + nome);
+
+        scanner.close();
     }
 }
