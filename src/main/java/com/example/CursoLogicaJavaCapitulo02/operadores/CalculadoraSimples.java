07428bd8b3180dc7188dd4dcc5abc184c0cbf43c
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-01-19 14:19:23 -0300

    Criado código para fazer uma calculadora simples no Java. Exercicio1.Aula04

---
+++ src/main/java/com/example/CursoLogicaJavaCapitulo02/operadores/CalculadoraSimples.java
@@ -0,0 +1,36 @@
+package com.example.CursoLogicaJavaCapitulo02.operadores;
+
+import java.util.Scanner;
+
+public class CalculadoraSimples {
+
+    public static void main (String [] args){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite o primeiro número: ");
+        Double num1 = scanner.nextDouble();
+
+        System.out.print("Escolha a operação a fazer: (1) Soma, (2)Substraçao, (3) Multiplicação e (4) divisão : ");
+        Integer escolhaOperacao = scanner.nextInt();
+//        Integer escolhaOperacao = 0;
+
+        System.out.print("Digite o segundo número: ");
+        Double num2 = scanner.nextDouble();
+
+
+        if(escolhaOperacao == 1){
+          Double soma = num1 + num2;
+          System.out.println("O resultado da soma é: " + soma);
+        }else if(escolhaOperacao == 2){
+         Double subtracao = num1 - num2;
+         System.out.println("O resultado da subtração é: " + subtracao);
+        }else if(escolhaOperacao == 3){
+            Double multiplicacao = num1 * num2;
+            System.out.println("O resultado da multiplicacao é: " + multiplicacao);
+        }else if(escolhaOperacao == 4){
+            Double divisao = num1 / num2;
+            System.out.println("O resultado da subtração é: " + divisao);
+        }
+        scanner.close();
+    }
+}
