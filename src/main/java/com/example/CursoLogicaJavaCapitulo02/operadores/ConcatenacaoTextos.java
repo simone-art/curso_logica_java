87acc3e92ec777700e36d60883b0ad948328df32
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-01-20 16:17:44 -0300

    Concatenação de textos com uso de variáveis e números

--- src/main/java/com/example/CursoLogicaJavaCapitulo02/operadores/ConcatenacaoTextos.java
+++ src/main/java/com/example/CursoLogicaJavaCapitulo02/operadores/ConcatenacaoTextos.java
@@ -3,13 +3,14 @@ package com.example.CursoLogicaJavaCapitulo02.operadores;
 public class ConcatenacaoTextos {
 
     public static void main (String [] args){
-        String inicio = "Esse ";
-        String meio = "é um exemplo ";
+        String inicio = "Esse é o ";
+        String meio = " exemplo ";
         String fim = "de concatenação de textos. ";
+        Integer numeroExemplo = 1;
 
-        System.out.println(inicio + meio + fim);
+        System.out.println(inicio + numeroExemplo+ meio + fim);
 
         //"Esse" é considerado um texto estático.Texto que se usa sem ter uma variável
-        System.out.println("Esse " + meio + "de concatenação de textos. ");
+//        System.out.println("Esse " + meio + "de concatenação de textos. ");
     }
 }
