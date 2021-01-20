79455f9aa7c60782968f314c93db9abff45fd33b
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-01-20 16:40:39 -0300

    Criado código para incrementar cálculo usando ++

---
+++ src/main/java/com/example/CursoLogicaJavaCapitulo02/operadores/OperadoresIncrementoDecremento.java
@@ -0,0 +1,14 @@
+package com.example.CursoLogicaJavaCapitulo02.operadores;
+
+public class OperadoresIncrementoDecremento {
+
+    public static void main (String [] args){
+        Integer numero = 10;
+        System.out.println("Numero antes: " + numero);
+
+        //++numero;
+        numero++;
+        //--numero;
+        System.out.println("Numero depois: " + numero);
+    }
+}
