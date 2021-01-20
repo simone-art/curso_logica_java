6dde7ea1b39a6909cda496af19cf27e74e670b45
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-01-20 14:10:05 -0300

    Usado operador lógico Ou || para a tomada de decisão

--- src/main/java/com/example/CursoLogicaJavaCapitulo02/operadores/OperadoresLogicos.java
+++ src/main/java/com/example/CursoLogicaJavaCapitulo02/operadores/OperadoresLogicos.java
@@ -4,22 +4,26 @@ public class OperadoresLogicos {
 
     //Operador Lógico E &&
     public static void main(String[] args) {
-        Boolean carrinhoMaiorQue100 = true;
-        Boolean periodoDePromocao = true;
+//        Boolean carrinhoMaiorQue100 = false;
+//        Boolean periodoDePromocao = false;
 
-        Boolean aplicarDesconto = carrinhoMaiorQue100 && periodoDePromocao;
-        if(aplicarDesconto){
-            System.out.print("Sim! Aplique o desconto");
-        } else{
-            System.out.print("Não aplique o desconto!");
-        }
-        //Operador Lógico 0u ||
-
-//        Boolean aplicarDesconto = carrinhoMaiorQue100 || periodoDePromocao;
+//        Boolean aplicarDesconto = carrinhoMaiorQue100 && periodoDePromocao;
 //        if(aplicarDesconto){
 //            System.out.print("Sim! Aplique o desconto");
 //        } else{
 //            System.out.print("Não aplique o desconto!");
-//            }
+//        }
+        //Operador Lógico 0u ||
+        // Se colocar os dois false, printará não tela que não vai aplicar desconto
+        //Se colocar um false e outro true, printará na tela que vai aplicar o desconto
+        Boolean carrinhoMaiorQue100 = false;
+        Boolean periodoDePromocao = false;
+
+        Boolean aplicarDesconto = carrinhoMaiorQue100 || periodoDePromocao;
+        if(aplicarDesconto){
+            System.out.print("Sim! Aplique o desconto");
+        } else{
+            System.out.print("Não aplique o desconto!");
+            }
     }
 }
