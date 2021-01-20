ce7dd823d20d6437a251d249b97d43c0e1fde2be
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-01-20 11:57:52 -0300

    Criado programa para calcular gastoas familiares usando os operadores de atribuicão

--- src/main/java/com/example/CursoLogicaJavaCapitulo02/operadores/CalculoGastosFamiliar.java
+++ src/main/java/com/example/CursoLogicaJavaCapitulo02/operadores/CalculoGastosFamiliar.java
@@ -19,23 +19,31 @@ public class CalculoGastosFamiliar {
         System.out.print("Digite o valor da conta de luz: ");
         Double valorContaDeLuz = scanner.nextDouble();
 
+
         System.out.print("Digite o valor da conta de agua: ");
         Double valorContaDeAgua = scanner.nextDouble();
+        valorContaDeAgua += valorContaDeLuz;
 
         System.out.print("Digite o valor da conta de telefone: ");
         Double valorContaDeTelefone = scanner.nextDouble();
+        valorContaDeTelefone += valorContaDeAgua;
 
         System.out.print("Digite o valor da escola do seu filho (a): ");
         Double valorContaEscola = scanner.nextDouble();
+        valorContaEscola += valorContaDeTelefone;
 
         System.out.print("Digite o valor da sua fatura do cartão de crédito: ");
         Double valorContaCartaoCredito = scanner.nextDouble();
+        valorContaCartaoCredito += valorContaEscola;
 
         System.out.print("Digite o valor da conta do supermercado: ");
         Double valorContaSupermercado = scanner.nextDouble();
+        valorContaSupermercado += valorContaCartaoCredito;
 
-        Double valorGastoFamiliar = valorContaDeLuz + valorContaDeAgua + valorContaDeTelefone + valorContaEscola + valorContaSupermercado + valorContaCartaoCredito;
-        System.out.print("O valor total do gasto familiar é de: " + valorGastoFamiliar);
+//        Double valorGastoFamiliar = valorContaDeLuz + valorContaDeAgua + valorContaDeTelefone + valorContaEscola + valorContaSupermercado + valorContaCartaoCredito;
+        System.out.print("O valor total do gasto familiar é de: " + valorContaSupermercado);
+
+        scanner.close();
         
     }
 }
