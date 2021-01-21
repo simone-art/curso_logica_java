ae522d2d9ab3663776d978b12a87eb76ce3d67b9
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-01-21 14:32:20 -0300

    Criado código com valor booleano para ser usado na estrutura do If

--- src/main/java/com/example/CursoLogicaJavaCapitulo02/estructura/EstruturaIf.java
+++ src/main/java/com/example/CursoLogicaJavaCapitulo02/estructura/EstruturaIf.java
@@ -3,15 +3,24 @@ package com.example.CursoLogicaJavaCapitulo02.estructura;
 public class EstruturaIf {
 
     public static void main (String [] args){
-        Boolean movimentaPeloMenosMetadeDoValor = true;
+
+        Double emprestimo = 4000.0;
+        Double movimentacaoMedia = 2000.0;
+
+        Boolean movimentaPeloMenosMetadeDoValor = (movimentacaoMedia * 2) >= emprestimo;
         Boolean aContaTemTempoSuficienteDeAbertura = true;
-        Boolean temNomeLimpo = true;
+        Boolean temNomeLimpo = false;
 
         // variável LiberarEmprestimo que almacena as tres condicionais
         Boolean liberarEmprestimo = movimentaPeloMenosMetadeDoValor
                 && aContaTemTempoSuficienteDeAbertura && temNomeLimpo;
-        if(liberarEmprestimo){
+
+        //Toda expresão que retorna um valor booleano pode ser usada na estrutura If
+        if((movimentacaoMedia * 2) >= emprestimo && aContaTemTempoSuficienteDeAbertura
+        && temNomeLimpo){
             System.out.println("Sim! Pode liberar empréstimo. ");
+        }else{
+            System.out.println("Não pode liberar o empréstimo. ");
         }
     }
 }
