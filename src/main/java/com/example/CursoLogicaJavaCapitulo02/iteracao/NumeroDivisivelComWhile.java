60b9efe7f8c1a6adae5ab473cc89e56afbf6186f
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-01-22 16:34:22 -0300

    Criado código com while que funciona corretamente.

--- src/main/java/com/example/CursoLogicaJavaCapitulo02/iteracao/NumeroDivisivelComWhile.java
+++ src/main/java/com/example/CursoLogicaJavaCapitulo02/iteracao/NumeroDivisivelComWhile.java
@@ -10,24 +10,18 @@ public class NumeroDivisivelComWhile {
     // Toda vez que o módulo de um número por 3 for igual a zero,
     // então esse número e divisível por 3.
 
-    static final Integer DIVISOR = 3;
 
     public static void main(String[] args) {
 
         Integer[] numeros = new Integer[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
-
         int i = 0;
         while(i < numeros.length){
             Integer numero = numeros[i];
-            Integer moduloPorTres = numero % DIVISOR;
-
-            if(moduloPorTres.equals(0)){
+            if( numero % 3 == 0){
                 System.out.println( numero + " é um número divisível por 3");
+                i++;
             }
             i++;
         }
-        System.out.println("Fim.");
     }
-
-
 }
