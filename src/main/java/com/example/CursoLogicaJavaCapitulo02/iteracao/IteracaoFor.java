c5ef044e6a4a3522d35da3c66953d26e3be32e28
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-01-22 13:28:02 -0300

    Código usando a iteração for mais o break

--- src/main/java/com/example/CursoLogicaJavaCapitulo02/iteracao/IteracaoFor.java
+++ src/main/java/com/example/CursoLogicaJavaCapitulo02/iteracao/IteracaoFor.java
@@ -6,17 +6,26 @@ public class IteracaoFor {
     // A estrutura i<5 retorna um valor booleano!
 
 
-    public static void main (String [] args){
+    public static void main(String[] args) {
 //        for(int i = 0; i<5; i++){
 //            System.out.println(i + " Imprima qualquer frase");
 //        }
 
-        Double [] carrinhoDeCompras = new Double[] {50.0, 50.0, 50.0};
-        Double total = 0.0;
-        for (int i = 0; i < carrinhoDeCompras.length; i++) {
-            total = total + carrinhoDeCompras[i];
-            System.out.println("Iteracao" + i + "," + "Total: " + total);
+//        Double [] carrinhoDeCompras = new Double[] {50.0, 50.0, 50.0};
+//        Double total = 0.0;
+//        for (int i = 0; i < carrinhoDeCompras.length; i++) {
+//            total = total + carrinhoDeCompras[i];
+//            System.out.println("Iteracao" + i + "," + "Total: " + total);
+//        }
+//        System.out.println("O total da compra é: " + total);
+
+        //Com o uso do break, você para o código
+        for (int i = 0; i < 10; i++) {
+            if(i== 5){
+                System.out.println("Vai parar!");
+                break;
+            }
+            System.out.println("Iteracao" + i);
         }
-        System.out.println("O total da compra é: " + total);
     }
 }
