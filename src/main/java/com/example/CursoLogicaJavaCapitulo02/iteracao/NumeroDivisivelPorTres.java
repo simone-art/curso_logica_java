9e5ffbc5c403d01bd51d3d7a9dc8a575916fc837
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-01-22 14:15:46 -0300

    Criado código para calcular um número divisível por 3 e printar mensagem no console

---
+++ src/main/java/com/example/CursoLogicaJavaCapitulo02/iteracao/NumeroDivisivelPorTres.java
@@ -0,0 +1,24 @@
+package com.example.CursoLogicaJavaCapitulo02.iteracao;
+
+public class NumeroDivisivelPorTres {
+
+    //Dada a lista de números abaixo:
+
+    //Integer[] numeros = new Integer[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
+
+    //Itere por essa lista e imprima todos os números que são divisíveis por 3.
+    //Para descobrir se um número é divisível por 3, você pode utilizar o operador módulo.
+    // Toda vez que o módulo de um número por 3 for igual a zero,
+    // então esse número e divisível por 3.
+
+    public static void main(String[] args) {
+
+        Integer[] numeros = new Integer[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
+        for (int i = 0; i < numeros.length; i++) {
+            Integer numero = numeros[i];
+            if (numero % 3 == 0) {
+                System.out.println(numero + " é divisível por 3");
+            }
+        }
+    }
+}
