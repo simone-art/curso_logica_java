81dab191a198e06a53ce18a01adf4a9a93213921
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-01-22 17:21:56 -0300

    Código para subtituir item de uma lista usando vector de uma dimensão

--- src/main/java/com/example/CursoLogicaJavaCapitulo02/vetores/Vetores1Dimensao.java
+++ src/main/java/com/example/CursoLogicaJavaCapitulo02/vetores/Vetores1Dimensao.java
@@ -3,21 +3,29 @@ package com.example.CursoLogicaJavaCapitulo02.vetores;
 import java.util.Scanner;
 
 public class Vetores1Dimensao {
-    public static void main (String [] args){
-        String [] cardapio = new String[]{"Calabresa", "Atum", "Queijo", "Presunto"};
+    public static void main(String[] args) {
+//        String [] cardapio = new String[]{"Calabresa", "Atum", "Queijo", "Presunto"};
+//
+//        System.out.println("Escolha o sabor: ");
+//        for(int i = 0; i <cardapio.length; i++){
+//            System.out.println("[" + i+ "]" +cardapio[i]);
+//        }
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.print("Digite o número referente ao sabor: ");
+//        Integer saborEscolhido = scanner.nextInt();
+//
+//        System.out.println("Você escolheo o sabor: " + cardapio[saborEscolhido]);
+//        scanner.close();
+//
+//        }
 
-        System.out.println("Escolha o sabor: ");
-        for(int i = 0; i <cardapio.length; i++){
-            System.out.println("[" + i+ "]" +cardapio[i]);
+        //Alterando um item da lista para adicionar outro em seu lugar
+        String[] cardapio = new String[]{"Calabresa", "Atum", "Queijo", "Presunto"};
+        cardapio[3] = "Frango";
+
+        for (int i = 0; i < cardapio.length; i++) {
+            System.out.println(cardapio[i]);
         }
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Digite o número referente ao sabor: ");
-        Integer saborEscolhido = scanner.nextInt();
 
-        System.out.println("Você escolheo o sabor: " + cardapio[saborEscolhido]);
-        scanner.close();
-
-        }
-
+    }
 }
-
