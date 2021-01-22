cef4fd5b022057d112f23f5a9f96a5b86c895dc0
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-01-22 12:51:14 -0300

    Criado código básico para usar o laço de iteração for

---
+++ src/main/java/com/example/CursoLogicaJavaCapitulo02/iteracao/IteracaoFor.java
@@ -0,0 +1,14 @@
+package com.example.CursoLogicaJavaCapitulo02.iteracao;
+
+public class IteracaoFor {
+
+    //Iterando com o laço for
+    // A estrutura i<5 retorna um valor booleano!
+
+
+    public static void main (String [] args){
+        for(int i = 0; i<5; i++){
+            System.out.println(i + " Imprima qualquer frase");
+        }
+    }
+}
