6a8609229dbbc1d381b139c556d5c1844afdca3c
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-01-22 16:29:15 -0300

    Código do curso porque o meu estava dando erro. Já vi qual era o erro

--- src/main/java/com/example/CursoLogicaJavaCapitulo02/iteracao/NumeroDivisivelComWhile.java
+++ src/main/java/com/example/CursoLogicaJavaCapitulo02/iteracao/NumeroDivisivelComWhile.java
@@ -14,17 +14,20 @@ public class NumeroDivisivelComWhile {
 
     public static void main(String[] args) {
 
-
         Integer[] numeros = new Integer[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
+
         int i = 0;
         while(i < numeros.length){
             Integer numero = numeros[i];
             Integer moduloPorTres = numero % DIVISOR;
-            if( moduloPorTres.equals(0)){
-                System.out.println( i + " é um número divisível por 3");
+
+            if(moduloPorTres.equals(0)){
+                System.out.println( numero + " é um número divisível por 3");
             }
             i++;
         }
         System.out.println("Fim.");
     }
+
+
 }
