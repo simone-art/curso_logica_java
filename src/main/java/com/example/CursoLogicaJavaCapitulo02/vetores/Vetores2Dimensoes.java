bfd034afcdab466cfac6142dad010af6d32e4b9f
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-01-25 11:11:21 -0300

    Códigp feito para printar no console os valores do faturamento de cada mês

--- src/main/java/com/example/CursoLogicaJavaCapitulo02/vetores/Vetores2Dimensoes.java
+++ src/main/java/com/example/CursoLogicaJavaCapitulo02/vetores/Vetores2Dimensoes.java
@@ -19,7 +19,20 @@ public class Vetores2Dimensoes {
 
 
         // Para que o vector funcione, devemos accesá-lo da seguinte usando a posicão:
-        System.out.println("Faturamento do día 1 de janeiro: " + faturamentoAnual [0][0]);
-        System.out.println("Faturamento do día 3 de fevereiro: " + faturamentoAnual [1][2]);
+//        System.out.println("Faturamento do día 1 de janeiro: " + faturamentoAnual [0][0]);
+//        System.out.println("Faturamento do día 3 de fevereiro: " + faturamentoAnual [1][2]);
+
+        //Printa o mês contido na lista fatruamentoAnual
+        for (int i = 0; i <faturamentoAnual.length; i++){
+            System.out.println("Mês: " + (i +1));
+
+            //Printa o valor do faturamento diário de cada mês
+            Double[] mes = faturamentoAnual[i];
+        for(int y = 0; y < mes.length; y++){
+            Double dia = mes[y]; // Double dia = faturamentoAnual[i][y];
+            System.out.println("Dia " + (y + 1) + ":" + dia);
+        }
+
+        }
     }
 }
