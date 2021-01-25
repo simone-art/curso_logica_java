b7301e54a0b380b2b5de4b14198676580c5bfecc
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-01-25 15:51:33 -0300

    Vetores de mais de tres dimensões

--- src/main/java/com/example/CursoLogicaJavaCapitulo02/vetores/SemanaMaiorFaturamento.java
+++ src/main/java/com/example/CursoLogicaJavaCapitulo02/vetores/SemanaMaiorFaturamento.java
@@ -12,14 +12,14 @@ public class SemanaMaiorFaturamento {
     //Crie um algoritmo que encontre qual a semana teve o maior faturamento.
     // No final mostre qual foi essa semana e de quanto foi esse faturamento.
 
-    public static void main (String [] args){
+    public static void main(String[] args) {
 
-        Double[] semanaUm = new Double[]{ 1000.0, 1500.0, 1250.0, 2100.0, 1350.0, 970.0, 0.0 };
-        Double[] semanaDois = new Double[]{ 1050.0, 2500.0, 3250.0, 1100.0, 1430.0, 900.0, 0.0 };
-        Double[] semanaTres = new Double[]{ 1700.0, 1300.0, 1450.0, 2000.0, 1390.0, 800.0, 0.0 };
-        Double[] semanaQuatro = new Double[]{ 2100.0, 1600.0, 1850.0, 2100.0, 1220.0, 998.0, 0.0 };
+        Double[] semanaUm = new Double[]{1000.0, 1500.0, 1250.0, 2100.0, 1350.0, 970.0, 0.0};
+        Double[] semanaDois = new Double[]{1050.0, 2500.0, 3250.0, 1100.0, 1430.0, 900.0, 0.0};
+        Double[] semanaTres = new Double[]{1700.0, 1300.0, 1450.0, 2000.0, 1390.0, 800.0, 0.0};
+        Double[] semanaQuatro = new Double[]{2100.0, 1600.0, 1850.0, 2100.0, 1220.0, 998.0, 0.0};
 
-        Double[][] mes = new Double[][] { semanaUm, semanaDois, semanaTres, semanaQuatro };
+        Double[][] mes = new Double[][]{semanaUm, semanaDois, semanaTres, semanaQuatro};
 
         Double totalSemanalFaturamento = semanaUm[0] + semanaUm[1] + semanaUm[2] + semanaUm[3] + semanaUm[4] + semanaUm[5] + semanaUm[6];
         System.out.println("O total da semana Um é: " + totalSemanalFaturamento);
@@ -44,6 +44,7 @@ public class SemanaMaiorFaturamento {
          }else if(totalSemanalFaturamentoQuatro >= totalSemanalFaturamento || totalSemanalFaturamentoQuatro>= totalSemanalFaturamentoDois || totalSemanalFaturamentoQuatro>= totalSemanalFaturamentoTres){
              System.out.println("A semana quatro é a de maior venda com um total de " + totalSemanalFaturamentoTres);
          }
-    }
 
+
+    }
 }
---
+++ src/main/java/com/example/CursoLogicaJavaCapitulo02/vetores/VetoresDeMaisDeDuasDimensoes.java
@@ -0,0 +1,28 @@
+package com.example.CursoLogicaJavaCapitulo02.vetores;
+
+public class VetoresDeMaisDeDuasDimensoes {
+
+    //Vetores de duas dimensões para que funcionen, deve existir um vector de uma dimensão
+    // O valor do vector de duas dimensões precisa ser um vector de uma dimensão
+
+    public static void main (String [] args){
+
+        //Vectores de uma dimensão                  1500=[0]==
+        Double [] faturamentoJaneiro = new Double[] {1500.0, 2000.0, 7000.0, };
+        Double [] faturamentoFevereiro = new Double[] {700.0, 1200.0, 4000.0 };
+
+        //Vector de duas dimensões                      ====== [0]========
+        Double [] [] faturamentoAnual = new Double[] [] {faturamentoJaneiro, faturamentoFevereiro};
+
+       //Vector de tres dimensões
+        //No vector de 3 só cabe um vector de duas dimensões e assim sucessivamente
+        Double [] [] [] faturamentoQuinquenal = new Double[][][]{faturamentoAnual};
+
+        //Vector de quatro dimensões
+        //No vector de 4 só cabe um vector de tres dimensões e assim sucessivamente
+        Double [] [] [] [] faturamentoDaDecada = new Double[][][] []{faturamentoQuinquenal};
+
+
+        }
+    }
+}
