3a93360405d0fdb28e3369f22b8e62cee40fe95e
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-01-26 12:23:46 -0300

    Criado método para retornar valores

---
+++ src/main/java/com/example/CursoLogicaJavaCapitulo02/metodos/RetornandoValores.java
@@ -0,0 +1,14 @@
+package com.example.CursoLogicaJavaCapitulo02.metodos;
+
+public class RetornandoValores {
+
+    public static void main (String [] args){
+       Integer numeroQualquer = numeroAleatorio();
+       System.out.println("Número: " + numeroQualquer);
+    }
+
+    static Integer numeroAleatorio(){
+        Integer numero = 1;
+        return numero;
+    }
+}
