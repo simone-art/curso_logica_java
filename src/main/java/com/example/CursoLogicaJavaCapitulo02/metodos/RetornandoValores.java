0562a7da9f8e0e518687d8fa028d7ca00e85f612
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-01-26 13:05:21 -0300

    Adicionando outros métodos com retorno de valores

--- src/main/java/com/example/CursoLogicaJavaCapitulo02/metodos/RetornandoValores.java
+++ src/main/java/com/example/CursoLogicaJavaCapitulo02/metodos/RetornandoValores.java
@@ -1,8 +1,10 @@
 package com.example.CursoLogicaJavaCapitulo02.metodos;
 
+import java.util.Scanner;
+
 public class RetornandoValores {
 
-    public static void main (String [] args){
+    public static void main(String[] args) {
 //       Integer numeroQualquer = numeroAleatorio();
 //       System.out.println("Número: " + numeroQualquer);
 //    }
@@ -16,17 +18,96 @@ public class RetornandoValores {
         // 50 -------- 100%
         //  --------   20%
 
-    Double [ ] premissaInicial = new Double[]{725.5, 100.0};
-    Double proporcao = 20.0;
+//    Double [ ] premissaInicial = new Double[]{725.5, 100.0};
+//    Double proporcao = 20.0;
+//
+//    Double resultadoFinal = regraDeTres(premissaInicial, proporcao);
+//
+//    System.out.println("O resultado é: " + resultadoFinal);
+//
+//    }
+//
+//    static Double regraDeTres(Double[] premissaInicial, Double proporcao){
+//        Double resultadoFinal = (premissaInicial[0] * proporcao)/ premissaInicial[1];
+//        return resultadoFinal;
+//    }
 
-    Double resultadoFinal = regraDeTres(premissaInicial, proporcao);
+            Scanner scanner = new Scanner(System.in);
 
-    System.out.println("O resultado é: " + resultadoFinal);
+            imprimirTraco();
 
+            String[] cursos = new String[]{"Java EE", "Spring", "Java OO Avançado"};
+
+            imprimir("Escolha dentre os cursos abaixo: ");
+
+            iterarEExibirPosicoesDoVetorString(cursos);
+
+            Integer posicaoCursoEscolhido = receberNumeroInteiroDoUsuario("O curso que você deseja é o: ", scanner);
+
+            Boolean posicaoValida = verificarPosicaoEscolhidaPeloUsuario(posicaoCursoEscolhido, cursos);
+
+            if (!posicaoValida) {
+                encerrarProgramaPorCausaDaPosicaoInvalida();
+            }
+
+            imprimirTraco();
+
+            String[] formasPagamento = new String[]{"Cartão", "Boleto"};
+
+            imprimir("Escolha dentre as formas de pagamento abaixo: ");
+
+            iterarEExibirPosicoesDoVetorString(formasPagamento);
+
+            Integer posicaoFormaPagamentoEscolhida = receberNumeroInteiroDoUsuario("Sua forma de pagamento escolhida é: ", scanner);
+
+            posicaoValida = verificarPosicaoEscolhidaPeloUsuario(posicaoFormaPagamentoEscolhida, formasPagamento);
+
+            if (!posicaoValida) {
+                encerrarProgramaPorCausaDaPosicaoInvalida();
+            }
+
+            String cursoEscolhido = cursos[posicaoCursoEscolhido];
+            String formaPagamentoEscolhida = formasPagamento[posicaoFormaPagamentoEscolhida];
+
+            imprimirTraco();
+
+            imprimir("O curso escolhido foi " + cursoEscolhido + " e a forma de pagamento é " + formaPagamentoEscolhida);
+
+            scanner.close();
+        }
+
+        static Integer receberNumeroInteiroDoUsuario (String texto, Scanner scanner){
+            imprimirEContinuarNaMesmaLinha(texto);
+            Integer numero = scanner.nextInt();
+
+            return numero;
+        }
+
+        static Boolean verificarPosicaoEscolhidaPeloUsuario (Integer posicao, String[]vetor){
+            Boolean valida = posicao >= 0 && posicao < vetor.length;
+            return valida;
+        }
+
+        static void imprimir (String texto){
+            System.out.println(texto);
+        }
+
+        static void imprimirEContinuarNaMesmaLinha (String texto){
+            System.out.print(texto);
+        }
+
+        static void iterarEExibirPosicoesDoVetorString (String[]vetor){
+            for (int i = 0; i < vetor.length; i++) {
+                imprimir("[" + i + "] " + vetor[i]);
+            }
+        }
+
+        static void imprimirTraco () {
+            imprimir("----------------------------------------------");
+        }
+
+        static void encerrarProgramaPorCausaDaPosicaoInvalida () {
+            System.err.println("Posição inválida!");
+            System.exit(1);
+        }
     }
-
-    static Double regraDeTres(Double[] premissaInicial, Double proporcao){
-        Double resultadoFinal = (premissaInicial[0] * proporcao)/ premissaInicial[1];
-        return resultadoFinal;
-    }
-}
