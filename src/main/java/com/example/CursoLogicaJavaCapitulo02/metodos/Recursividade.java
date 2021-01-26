9e5105cc81781be3f06ba803982fab59172683c3
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-01-26 16:46:47 -0300

    Criado código para usar a recursividade iterando uma lista de nomes de alunos

--- src/main/java/com/example/CursoLogicaJavaCapitulo02/metodos/Recursividade.java
+++ src/main/java/com/example/CursoLogicaJavaCapitulo02/metodos/Recursividade.java
@@ -2,17 +2,25 @@ package com.example.CursoLogicaJavaCapitulo02.metodos;
 
 public class Recursividade {
     //Recursividade é chamar o mesmo método
-    
+
     public static void main (String [] args){
 
-        imprimirNumero(0);
+//        imprimirNumero(0);
+        String [] alunos = new String[]{"Simone", "Diego", "Marcos"};
+        iterarNomes(alunos);
     }
 
-    static void imprimirNumero(Integer numero){
-        System.out.println("Número: " + numero);
+//    static void imprimirNumero(Integer numero){
+//        System.out.println("Número: " + numero);
+//
+//        if(numero < 10){
+//            imprimirNumero(++numero);
+//        }
+//    }
 
-        if(numero < 10){
-            imprimirNumero(++numero);
+    static void iterarNomes(String []alunos){
+        for(int i = 0; i < alunos.length; i++){
+            System.out.println("Alunos: " + alunos[i]);
         }
     }
 }
