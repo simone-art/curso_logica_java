3e948a4219654c6aa804e06ffecc5f9a2719c134
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-01-26 12:40:37 -0300

    Adicionado código com métodos que retornan valores

--- src/main/java/com/example/CursoLogicaJavaCapitulo02/metodos/RetornandoValores.java
+++ src/main/java/com/example/CursoLogicaJavaCapitulo02/metodos/RetornandoValores.java
@@ -3,12 +3,30 @@ package com.example.CursoLogicaJavaCapitulo02.metodos;
 public class RetornandoValores {
 
     public static void main (String [] args){
-       Integer numeroQualquer = numeroAleatorio();
-       System.out.println("Número: " + numeroQualquer);
+//       Integer numeroQualquer = numeroAleatorio();
+//       System.out.println("Número: " + numeroQualquer);
+//    }
+//
+//    //Para usar o return não se pode usar a palavra reservada void
+//    //O void não permite retornar valores
+//    static Integer numeroAleatorio(){
+//        Integer numero = 1;
+//        return numero;
+
+        // 50 -------- 100%
+        //  --------   20%
+
+    Double [ ] premissaInicial = new Double[]{50.0, 100.0};
+    Double proporcao = 20.0;
+
+    Double resultadoFinal = regraDeTres(premissaInicial, proporcao);
+
+    System.out.println("O resultado é: " + resultadoFinal);
+
     }
 
-    static Integer numeroAleatorio(){
-        Integer numero = 1;
-        return numero;
+    static Double regraDeTres(Double[] premissaInicial, Double proporcao){
+        Double resultadoFinal = (premissaInicial[0] * proporcao)/ premissaInicial[1];
+        return resultadoFinal;
     }
 }
