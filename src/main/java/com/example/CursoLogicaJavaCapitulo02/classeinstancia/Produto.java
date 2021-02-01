d666c499218b0f58d7a7e5c75459332c55d73843
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-02-01 13:43:48 -0300

    "Criado programa que imprima, no console, a lista de tarefas que estão no arquivo tipo txt"

--- src/main/java/com/example/CursoLogicaJavaCapitulo02/classeinstancia/Produto.java
+++ src/main/java/com/example/CursoLogicaJavaCapitulo02/classeinstancia/Produto.java
@@ -3,5 +3,7 @@ package com.example.CursoLogicaJavaCapitulo02.classeinstancia;
 // Isto aquí é uma classe
 public class Produto {
 
+    static Integer quantidadeMinimaEstoque = 1;
+
     String nome;
 }
