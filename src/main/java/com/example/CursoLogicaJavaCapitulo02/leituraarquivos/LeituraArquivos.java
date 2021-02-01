332cf7ede8bb29b8e1dbc84e687eb0f35ec64a0b
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-02-01 13:18:33 -0300

    "Criado código oara lêr arquivo.txt e mostrar conteúdo no console"

---
+++ src/main/java/com/example/CursoLogicaJavaCapitulo02/leituraarquivos/LeituraArquivos.java
@@ -0,0 +1,20 @@
+package com.example.CursoLogicaJavaCapitulo02.leituraarquivos;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class LeituraArquivos {
+    public static void main (String[] args) throws IOException {
+        Path arquivo = Paths.get("C:/Users/s.santos.pimenta/Documents/txt/meu-arquivo.txt");
+        List<String> linhas = Files.readAllLines(arquivo);
+
+        for(int i = 0; i< linhas.size(); i++){
+            String nome = linhas.get(i);
+            System.out.println("Jogador " + i + ": " + nome);
+
+        }
+    }
+}
