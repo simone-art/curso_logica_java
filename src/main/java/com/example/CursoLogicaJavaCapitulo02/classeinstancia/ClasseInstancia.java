1ed41c2327145a7772861cb8aadf36ec0ab98da3
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-02-01 11:10:16 -0300

    "Criada classe e método estático para fazer impressão personalizada"

--- src/main/java/com/example/CursoLogicaJavaCapitulo02/classeinstancia/ClasseInstancia.java
+++ src/main/java/com/example/CursoLogicaJavaCapitulo02/classeinstancia/ClasseInstancia.java
@@ -3,13 +3,27 @@ package com.example.CursoLogicaJavaCapitulo02.classeinstancia;
 public class ClasseInstancia {
 
     public static void main (String [] args){
+
+        System.out.println("Quantidade mínima em estoque: " + Produto.quantidadeMinimaEstoque);
         Produto produto = new Produto();
+        //Variável estática aquí foi alterada
+        produto.quantidadeMinimaEstoque = 10;
         produto.nome = "Caneca";
+
 
         Produto produto1 = new Produto();
         produto1.nome = "Carteira";
 
+
         System.out.println("Nome do Produto : " + produto.nome);
         System.out.println("Nome do Produto : " + produto1.nome);
+        //Para printar no console a quantidade mínima, tem que chamar a classe e não a variável
+        //Já que temos ua variável estática que ao mesmo tempo não é constante
+        System.out.println("Quantidade mínima do produto em estoque: " + Produto.quantidadeMinimaEstoque);
+
+        //Métodos estáticos
+        Impressao.informacao("Produto: " + produto.nome);
+        Impressao.informacao("Produto1: " + produto1.nome);
+
     }
 }
---
+++ src/main/java/com/example/CursoLogicaJavaCapitulo02/classeinstancia/Impressao.java
@@ -0,0 +1,15 @@
+package com.example.CursoLogicaJavaCapitulo02.classeinstancia;
+
+public class Impressao {
+
+    //INFO AQUÍ GERA UMA IMPRESSÃO NO CONSOLE PERSONALIZADA
+    static void informacao(String texto){
+        System.out.println("[INFO]" + texto);
+    }
+
+    static void erro(String texto){
+        System.out.println("[ERRO]" + texto);
+    }
+
+
+}
