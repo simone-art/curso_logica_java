2b40eb588cfbf3ce360cd8a4ae9d90047cd67bfa
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-02-01 09:12:43 -0300

    "Criado código com métodos de instância"

--- src/main/java/com/example/CursoLogicaJavaCapitulo02/programacao/MetodosDeInstancia.java
+++ src/main/java/com/example/CursoLogicaJavaCapitulo02/programacao/MetodosDeInstancia.java
@@ -11,13 +11,13 @@ public class MetodosDeInstancia {
         //Depois com os atributos preenchidos, você cria um método para printar
         // o nome completo do cliente.
 
-        Cliente cliente = new Cliente();
-        cliente.primeiroNome = "Simone";
-        cliente.ultimoNome = "Santos";
-        cliente.telefone = "11 5555 2233";
-        cliente.email = "simsantos@algamorks.com.br";
-
-        System.out.println("Nome Completo: " + cliente.obterNomeCompletoCliente() + " DDD " + cliente.obterDDD());
+//        Cliente cliente = new Cliente();
+//        cliente.primeiroNome = "Simone";
+//        cliente.ultimoNome = "Santos";
+//        cliente.telefone = "11 5555 2233";
+//        cliente.email = "simsantos@algamorks.com.br";
+//
+//        System.out.println("Nome Completo: " + cliente.obterNomeCompletoCliente() + " DDD " + cliente.obterDDD());
 
     }
         //Métodos státicos não são métodos de instância
