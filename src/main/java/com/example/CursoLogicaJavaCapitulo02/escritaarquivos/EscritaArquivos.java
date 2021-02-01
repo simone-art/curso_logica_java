cea48f4ea194cae6b5f3b27e34bd175d52e8ee21
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-02-01 12:36:22 -0300

    "Criado código para ler nomes no console e aparecer no arquivo.txt"

--- src/main/java/com/example/CursoLogicaJavaCapitulo02/escritaarquivos/EscritaArquivos.java
+++ src/main/java/com/example/CursoLogicaJavaCapitulo02/escritaarquivos/EscritaArquivos.java
@@ -5,15 +5,23 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class EscritaArquivos {
 
     //Tem que ser lanzado o throws IOException I: Input 0:Output
     //Para que o Files deixe de estar em vermelho
     public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+
         ArrayList<String> linhas = new ArrayList<String>();
-        //add é um método que adiciona items a uma lista
-        linhas.add("Linha Um");
+
+        for(int i = 0; i< 5; i++) {
+            System.out.print("Jogador " + i + ": ");
+            String nome = scanner.nextLine();
+            //add é um método que adiciona items a uma lista
+            linhas.add(nome);
+        }
         Path arquivo = Paths.get("C:/Users/s.santos.pimenta/Documents/txt/meu-arquivo.txt");
         Files.write(arquivo, linhas);
         System.out.println("Fim...");
