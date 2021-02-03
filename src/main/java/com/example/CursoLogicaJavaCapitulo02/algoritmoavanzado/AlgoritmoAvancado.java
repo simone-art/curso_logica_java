45512fa4a8d2f6b94b5427b35a86f95ace1153af
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-02-03 16:00:05 -0300

    "Código para remover elementos de uma lista"

--- src/main/java/com/example/CursoLogicaJavaCapitulo02/algoritmoavanzado/AlgoritmoAvancado.java
+++ src/main/java/com/example/CursoLogicaJavaCapitulo02/algoritmoavanzado/AlgoritmoAvancado.java
@@ -9,21 +9,26 @@ public class AlgoritmoAvancado {
     aluno.nome = "Pedro";
     lista.adicionar(aluno);
 
-    aluno = new Aluno();
-    aluno.nome = "Alexandre";
-    lista.adicionar(aluno);
+    //aluno = new Aluno();
+    Aluno aluno1 = new Aluno();
+    aluno1.nome = "Alexandre";
+    lista.adicionar(aluno1);
 
-    aluno = new Aluno();
-    aluno.nome = "Solange";
-    lista.adicionar(aluno);
 
-    aluno = new Aluno();
-    aluno.nome = "Jhon";
-    lista.adicionar(aluno);
+    //aluno = new Aluno();
+    Aluno aluno2 = new Aluno();
+    aluno2.nome = "Solange";
+    lista.adicionar(aluno2);
 
-    aluno = new Aluno();
-    aluno.nome = "Thiago";
-    lista.adicionar(aluno);
+    //aluno = new Aluno();
+    Aluno aluno3 = new Aluno();
+    aluno3.nome = "Jhon";
+    lista.adicionar(aluno3);
+
+    //aluno = new Aluno();
+    Aluno aluno4 = new Aluno();
+    aluno4.nome = "Thiago";
+    lista.adicionar(aluno4);
 
     //Agora é neccesário iterar na lista
 
@@ -31,5 +36,15 @@ public class AlgoritmoAvancado {
       Aluno a = lista.obter(i);
       System.out.println("Aluno: " + a.nome);
     }
+
+    System.out.println("===========================");
+
+    lista.remover(aluno2);
+
+    for (int i = 0; i < lista.tamanho(); i++) {
+      Aluno a = lista.obter(i);
+      System.out.println("Aluno: " + a.nome);
+    }
   }
+
 }
--- src/main/java/com/example/CursoLogicaJavaCapitulo02/algoritmoavanzado/ListaAlunos.java
+++ src/main/java/com/example/CursoLogicaJavaCapitulo02/algoritmoavanzado/ListaAlunos.java
@@ -30,4 +30,29 @@ public class ListaAlunos {
         lista[tamanhoLista] = aluno;
         tamanhoLista++;
     }
+
+    void remover(Aluno aluno){
+        for (int i = 0; i <tamanhoLista; i++){
+            Aluno a = lista[i];
+
+            if(a.equals(aluno)){
+                remover(i);
+                break;
+            }
+        }
+    }
+    void remover(int indice){
+        // Se a lista for {"A", "B", "C"}
+        //// Agora ao remover o item B ficará  {"A", "C"}
+        //Você remove o valor e a posição deixada por esse valor e tomada por o item seguinte a lista
+        int indiceInicial = indice + 1;
+        for(int i = indiceInicial; i<tamanhoLista; i++){
+            lista[i - 1] = lista[i];
+        }
+        //Tamanho da lista fica mais pequeno
+        tamanhoLista--;
+        //Limpa o espaço deixado pelo item removido
+        lista[tamanhoLista] = null;
+
+    }
 }
